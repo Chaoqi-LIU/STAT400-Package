b40cf05d331b616ea323a74d1fc7c9e0d0881694
Chaoqi LIU <60335143+Chaoqi-LIU@users.noreply.github.com>
2023-02-17 14:01:31 -0600

    Update stat400_package.py

--- stat400_package.py
+++ stat400_package.py
@@ -12,8 +12,8 @@ class NegativeBinominal:
         self.prob = p
     def mean(self): return self.r / self.prob
     def variance(self): return self.r * (1 - self.prob) / (self.prob ** 2)
-    def pmf(self, x): return Math.C(x - 1, self.r - 1) * (self.prob ** self.r) * ((1 - self.prob) ** (x - self.r))
-    def cdf(self, x): return sum(self.pmf(i) for i in range(self.r, x + 1))
+    def pdf(self, x): return Math.C(x - 1, self.r - 1) * (self.prob ** self.r) * ((1 - self.prob) ** (x - self.r))
+    def cdf(self, x): return sum(self.pdf(i) for i in range(self.r, x + 1))
     def mgf(self, t): return self.prob ** self.r / ((1 - (1 - self.prob) * math.exp(t)) ** self.r)
 
 class Binominal:
@@ -22,8 +22,8 @@ class Binominal:
         self.prob = prob
     def mean(self): return self.n * self.prob  
     def variance(self): return self.n * self.prob * (1 - self.prob)   
-    def pmf(self, x): return Math.C(self.n, x) * (self.prob ** x) * ((1 - self.prob) ** (self.n - x))  
-    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+    def pdf(self, x): return Math.C(self.n, x) * (self.prob ** x) * ((1 - self.prob) ** (self.n - x))  
+    def cdf(self, x): return sum(self.pdf(i) for i in range(x + 1))
     def mgf(self, t): return (self.prob * math.exp(t) + (1 - self.prob)) ** self.n
 
 class Geometric:
@@ -31,8 +31,8 @@ class Geometric:
         self.prob = p
     def mean(self): return 1 / self.prob
     def variance(self): return (1 - self.prob) / self.prob ** 2
-    def pmf(self, x): return self.prob * ((1 - self.prob) ** (x - 1))
-    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+    def pdf(self, x): return self.prob * ((1 - self.prob) ** (x - 1))
+    def cdf(self, x): return sum(self.pdf(i) for i in range(x + 1))
     def mgf(self, t): return self.prob / (1 - (1 - self.prob) * math.exp(t))
 
 class Hypergeometric:
@@ -43,8 +43,8 @@ class Hypergeometric:
         self.n = n
     def mean(self): return self.n * self.N1 / self.N
     def variance(self): return self.n * self.N1 / self.N * self.N2 / self.N * (self.N - self.n) / (self.N - 1)
-    def pmf(self, x): return Math.C(self.N1, x) * Math.C(self.N2, self.n - x) / Math.C(self.N, self.n)
-    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+    def pdf(self, x): return Math.C(self.N1, x) * Math.C(self.N2, self.n - x) / Math.C(self.N, self.n)
+    def cdf(self, x): return sum(self.pdf(i) for i in range(x + 1))
 
 class Multinominal:
     def __init__(self, n, probs):
@@ -53,15 +53,15 @@ class Multinominal:
         self.k = len(self.probs)
     def mean(self, idx): return self.n * self.probs[idx - 1]
     def variance(self, idx): return self.n * self.probs[idx - 1] * (1 - self.probs[idx - 1])
-    def pmf(self, xs): return Math.fact(self.n) / math.prod(Math.fact(x) for x in xs) * math.prod(self.probs[i] ** xs[i] for i in range(self.k))
+    def pdf(self, xs): return Math.fact(self.n) / math.prod(Math.fact(x) for x in xs) * math.prod(self.probs[i] ** xs[i] for i in range(self.k))
 
 class Poisson:
     def __init__(self, lam):
         self.lam = lam
     def mean(self): return self.lam
     def variance(self): return self.lam
-    def pmf(self, x): return (self.lam ** x) * math.exp(-self.lam) / Math.fact(x)
-    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+    def pdf(self, x): return (self.lam ** x) * math.exp(-self.lam) / Math.fact(x)
+    def cdf(self, x): return sum(self.pdf(i) for i in range(x + 1))
     def mgf(self, t): return math.exp(self.lam * (math.exp(t) - 1))
 
 class Normal:
@@ -70,5 +70,5 @@ class Normal:
         self.var = var
     def mean(self): return self.mu
     def variance(self): return self.var
-    def pmf(self, x): return (1 / math.sqrt(2 * math.pi * self.var)) * math.exp(- ((x - self.mu) ** 2) / (2 * self.var))
+    def pdf(self, x): return (1 / math.sqrt(2 * math.pi * self.var)) * math.exp(- ((x - self.mu) ** 2) / (2 * self.var))
     def cdf(self, z): return round(sp.integrate.quad(lambda w : (1 / math.sqrt(2 * math.pi)) * math.exp(-((w) ** 2) / 2), float('-inf'), z)[0], 4)
