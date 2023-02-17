8644d693b067de08d51d22de98734e4a948e02f3
Chaoqi LIU <60335143+Chaoqi-LIU@users.noreply.github.com>
2023-02-16 23:20:28 -0600

    Update stat400_package.py

--- stat400_package.py
+++ stat400_package.py
@@ -1,3 +1,5 @@
+import math
+
 class Math:
     def fact(n):
         ans = 1
@@ -23,4 +25,40 @@ class Binominal:
     def mean(self): return self.n * self.prob  
     def variance(self): return self.n * self.prob * (1 - self.prob)   
     def pmf(self, x): return Math.C(self.n, x) * (self.prob ** x) * ((1 - self.prob) ** (self.n - x))  
+    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+
+class Geometric:
+    def __init__(self, p):
+        self.prob = p
+    def mean(self): return 1 / self.prob
+    def variance(self): return (1 - self.prob) / self.prob ** 2
+    def pmf(self, x): return self.prob * ((1 - self.prob) ** (x - 1))
+    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+
+class Hypergeometric:
+    def __init__(self, N, N1, n):
+        self.N = N
+        self.N1 = N1
+        self.N2 = self.N - self.N1
+        self.n = n
+    def mean(self): return self.n * self.N1 / self.N
+    def variance(self): return self.n * self.N1 / self.N * self.N2 / self.N * (self.N - self.n) / (self.N - 1)
+    def pmf(self, x): return Math.C(self.N1, x) * Math.C(self.N2, self.n - x) / Math.C(self.N, self.n)
+    def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
+
+class Multinominal:
+    def __init__(self, n, probs):
+        self.n = n
+        self.probs = probs
+        self.k = len(self.probs)
+    def mean(self, idx): return self.n * self.probs[idx - 1]
+    def variance(self, idx): return self.n * self.probs[idx - 1] * (1 - self.probs[idx - 1])
+    def pmf(self, xs): return Math.fact(self.n) / math.prod(Math.fact(x) for x in xs) * math.prod(self.probs[i] ** xs[i] for i in range(self.k))
+
+class Poisson:
+    def __init__(self, lam):
+        self.lam = lam
+    def mean(self): return self.lam
+    def variance(self): return self.lam
+    def pmf(self, x): return (self.lam ** x) * math.exp(-self.lam) / Math.fact(x)
     def cdf(self, x): return sum(self.pmf(i) for i in range(x + 1))
